874467e1cda9d4ab501fdb2d645cbf9d04a1328e
Austin <abushey@umass.edu>
2022-11-08 17:53:51 -0500

    Testing express js server

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-import { createServer } from 'http';
 import { parse } from 'url';
- 
+import path from 'path';
+import express from 'express'
+
 function basicServer(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'}); // we're sending HTML                                                       
-    res.write('Hello World!<BR>'); //write a response to the client                                                                
-    const requestedURL = req.url;
-    console.log('helooooooooooooooooooooooooooooooooooooooooooooooooooooooo ' + requestedURL);
-    res.write(`URL is <B>${requestedURL+"foo"}</B><BR>`); // template string (backquote)                                                 
-    const parsedURL = parse(requestedURL, true);
-    const query = parsedURL.query;
-    const pathname = parsedURL.pathname;
-    res.write(`path is <B>${JSON.stringify(pathname)}</B><BR>`);
-    res.write(`query components are <B>${JSON.stringify(query)}</B><BR>`);
-    if (Number.parseInt(query.age) >= 21) {
-      res.write('DRINK UP!<BR>');
-    } else {
-      res.write('STAY SOBER KIDS!<BR>');
-    }
-    res.end(); //end the response                                                                                                  
+    // res.writeHead(200, {'Content-Type': 'text/html'}); // we're sending HTML                                                       
+    // res.write('Hello World!<BR>'); //write a response to the client                                                                
+    // const requestedURL = req.url;
+    // console.log('helooooooooooooooooooooooooooooooooooooooooooooooooooooooo ' + requestedURL);
+    // res.write(`URL is <B>${requestedURL+"foo"}</B><BR>`); // template string (backquote)                                                 
+    // const parsedURL = parse(requestedURL, true);
+    // const query = parsedURL.query;
+    // const pathname = parsedURL.pathname;
+    // res.write(`path is <B>${JSON.stringify(pathname)}</B><BR>`);
+    // res.write(`query components are <B>${JSON.stringify(query)}</B><BR>`);
+    // if (Number.parseInt(query.age) >= 21) {
+    //   res.write('DRINK UP!<BR>');
+    // } else {
+    //   res.write('STAY SOBER KIDS!<BR>');
+    // }
+    // res.end(); //end the response       
+    res.sendFile('forum.html', { root: path.dirname('') })                                                                                    
 }
- 
-// Start the server                                                                                                                
-createServer(basicServer).listen(80); //the server object listens on port 80
+
+const app = express()
+const port = 8080
+app.use(express.static(path.dirname('')));
+app.get('/', (req, res) => {(basicServer(req, res))});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
