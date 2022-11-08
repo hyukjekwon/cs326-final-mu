8ad049162063b5643f6c7fa42e9769a767cef1e5
hyukjekwon <hyukjekwon@gmail.com>
2022-11-08 15:50:48 -0500

    Added samples.

--- looper.js
+++ looper.js
@@ -1,37 +1,37 @@
 function init_key_presses() {
     document.addEventListener("keyup", (e) => {
         if (e.code === "Space") {
-            play_pause()
+            play_pause();
         }
     })
 }
 
 function init_master_buttons() {
-    const play = document.getElementById("play")
+    const play = document.getElementById("play");
     play.addEventListener("mouseup", (e) => {
-        play_pause()
+        play_pause();
     })
 }
 
 
 function play_pause() {
-    console.log("play/pause")
+    console.log("play/pause");
 }
 
 function init_sequences() {
-    const layer = document.getElementById("layer-control")
+    const layer = document.getElementById("layer-control");
     const sequences = document.getElementsByClassName("sequence");
     for (let sequence of sequences) {
         for (let i = 0; i < 16; i++) {
-            const interval = document.createElement("div")
-            interval.classList.add("interval")
-            sequence.appendChild(interval)
+            const interval = document.createElement("div");
+            interval.classList.add("interval");
+            sequence.appendChild(interval);
         }
     }
 }
 
 function init_all() {
-    init_key_presses()
-    init_sequences()
-    init_master_buttons()
+    init_key_presses();
+    init_sequences();
+    init_master_buttons();
 }
