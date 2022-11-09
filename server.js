c0102adbbab7ad577368b8de013436fbe98706ec
Austin <abushey@umass.edu>
2022-11-08 19:31:36 -0500

    Testing

--- server.js
+++ server.js
@@ -19,14 +19,16 @@ function basicServer(req, res) {
     //   res.write('STAY SOBER KIDS!<BR>');
     // }
     // res.end(); //end the response       
-    res.sendFile('forum.html', { root: path.dirname('') })                                                                                    
+    console.log("Sending File");
+    res.sendFile('forum.html', { root: path.dirname('') });                                                                              
 }
 
-const app = express()
-const port = 8080
+const app = express();
+const port = 80;
 app.use(express.static(path.dirname('')));
+console.log("Sending File");
 app.get('/', (req, res) => {(basicServer(req, res))});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT || port, () => {
+  console.log(`Example app listening on port ${port}`);
 })
