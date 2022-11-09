dbe42e873dd8806f58e5887769e3b1896cff0107
hyukjekwon <hyukjekwon@gmail.com>
2022-11-08 22:32:27 -0500

    Basic loop audio in place

--- layer.js
+++ layer.js
@@ -1,9 +1,16 @@
-class Looper {
+class Layer {
     constructor(sample) {
         this.sample = sample;
-        this.sequence = init_sequence(sample);
+        this.sequence = this.init_sequence();
+        this.pan = 50;
+        this.layer_volume = 50;
     }
-    init_sequence(sample_name) {
-        return {"sample": sample_name, "sequence": [...Array(15)].map(e=>0)};
+    init_sequence() {
+        return [...Array(15)].map(e=>0);
+    }
+    change_sample(new_sample) {
+        this.sample = new_sample;
     }
 }
+
+export {Layer};
--- looper.js
+++ looper.js
@@ -1,51 +1,68 @@
-let playing = false;
-let interval;
+import {Layer} from './layer.js';
+let l;
 
 class Looper {
     constructor() {
         // each sequence will contain the type of the sample along with a map for which notes are active
         // types of samples will just be the name of the file
         this.layers = [new Layer("kick.wav")];
+        this.bpm = 120;
+        this.master_volume = 75;
+        this.playing = false;
+        this.interval;
+        this.cursor = {"time": 0}; // obj to alter by reference
     }
     add_layer(sample) {
-        this.layers.add(new Layer(sample));
+        this.layers.push(new Layer(sample));
+    }
+    remove_layer(index) {
+        if (this.layers.length > 1) {
+            return this.layers.splice(index, 1);
+        }
+    }
+    play_interval(layers, cursor) {
+        const time = cursor["time"];
+        const metronome = new Tone.Player("/samples/metronome.wav").toDestination();
+        metronome.autostart = true;
+        layers.forEach(layer => {
+            if (layer.sequence[time]) {
+                const sample = new Tone.Player("/samples/" + layer.sample).toDestination();
+                sample.autostart = true;
+            }
+        });
+        console.log(time)
+        cursor["time"] = (time + 1) % 16;
+    }
+    play_pause() {
+        if (this.playing) {
+            console.log("pause");
+            clearInterval(this.interval);
+            this.playing = false;
+            this.cursor = {"time": 0};
+        } else {
+            console.log("play");
+            this.interval = setInterval(this.play_interval, Math.floor(this.bpm * 2.0833), this.layers, this.cursor);
+            this.playing = true;
+        }
     }
 }
 
-function init_key_presses() {
+function init_key_presses(l) {
     document.addEventListener("keyup", (e) => {
         if (e.code === "Space") {
-            play_pause();
+            l.play_pause();
         }
     })
 }
 
-function init_master_buttons() {
+function init_master_buttons(l) {
     const play = document.getElementById("play");
     play.addEventListener("mouseup", (e) => {
-        play_pause();
+        l.play_pause();
     })
 }
 
-
-function play_pause() {
-    console.log("play/pause");
-    if (playing) {
-        clearInterval(interval);
-        playing = false;
-    } else {
-        interval = setInterval(play_sample, 300, "./samples/Hihat.wav");
-        playing = true;
-    }
-}
-
-function play_sample(sample_name) {
-    const sample = new Tone.Player(sample_name).toDestination();
-    sample.restart();
-    sample.autostart = true;
-}
-
-function init_sequences() {
+function render_layers() {
     const layer = document.getElementById("layer-control");
     const sequences = document.getElementsByClassName("sequence");
     for (let sequence of sequences) {
@@ -57,6 +74,14 @@ function init_sequences() {
     }
 }
 
-init_key_presses();
-init_sequences();
-init_master_buttons();
+function init_all() {
+    Tone.start();
+    l = new Looper()
+    l.add_layer("hihat.wav")
+    console.log(l)
+    init_key_presses(l);
+    init_master_buttons();
+    render_layers();
+}
+
+init_all()
